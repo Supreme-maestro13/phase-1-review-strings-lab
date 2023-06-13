66f5fabbd17c0fc5ca68a4e3d3578fdcb65cc40a
Nadhamudin <nadhamudin.ismail@student.moringaschool.com>
2023-06-13 12:55:44 +0300

    Successfully Completed the Coding Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper!'; 
+   
+let welcomeMessage = "Welcome to Flatbook, " + "Grace Hopper!"; 
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "GRACE HOPPER!"
+
+let shortGreeting = "Welcome, " + 'G!';
+
